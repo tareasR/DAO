10cd33e995b7b062f2ef3c83ae16d10d771b825c
tareasR <tareasrojano@gmail.com>
2023-11-29 18:19:29 -0600

    Update CORS

--- src/main/java/mx/uv/App.java
+++ src/main/java/mx/uv/App.java
@@ -23,6 +23,22 @@ public class App
         //port(80);
         port(getHerokuAssignedPort());
 
+        options("/*", (request, response) -> {
+
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+
         get("/usuarios", (request, response)->{
             response.type("application/json");
             return gson.toJson(usuarios.values());
